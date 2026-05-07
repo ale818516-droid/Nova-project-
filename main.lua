324262b34690ee82af1ca928c5e334ef15c4a244
ale818516-droid <ale818516@gmail.com>
2026-05-07 16:06:09 -0600

    Actualizar main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 -- ENLACE DE MONETIZACIÓN ACTUALIZADO
-local NuevoLink = "https://direct-link.net/5492042/Uyekny3zE2P5"
+local NuevoLink = "https://link-hub.net/5492042/XS4p33zMPv8Z"
 
 -- COPIADO AUTOMÁTICO AL INICIAR
 setclipboard(NuevoLink)
