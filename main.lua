ac4e71ed7e8079f371e8dbb01c4a5ecc005851b0
ale818516-droid <ale818516@gmail.com>
2026-07-02 19:53:40 -0600

    Actualizar main.lua

--- main.lua
+++ main.lua
@@ -463,3 +463,4 @@ game:GetService("UserInputService").JumpRequest:Connect(function()
         game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
     end
 end)
+
